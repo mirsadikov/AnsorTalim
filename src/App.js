e9681c9f25db5d5e5a08db854fa52be110dbfe4f
Mirabror Sodikov <mirabror9545@gmail.com>
2021-09-23 10:44:53 +0500

    Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <nav className="nav">
+          <a href="/" className="nav__brand">
+            <img
+              src={logo}
+              alt="Ansor Ta'lim Logo"
+              className="nav__brand-logo"
+            />
+
+            <h1 className="nav__brand-name">Ansor Ta'lim</h1>
+          </a>
+          <ul className="nav__list">
+            <li>
+              <a href="#yangiliklar" className="nav__link subtitle">
+                Yangiliklar
+              </a>
+            </li>
+            <li>
+              <a href="#kurslar" className="nav__link subtitle">
+                Kurslar
+              </a>
+            </li>
+            <li>
+              <a href="#ustozlar" className="nav__link subtitle">
+                Ustozlar
+              </a>
+            </li>
+            <li>
+              <a href="#izohlar" className="nav__link subtitle">
+                Izohlar
+              </a>
+            </li>
+            <li>
+              <a href="#lavxalar" className="nav__link subtitle">
+                Darslardan lavxalar
+              </a>
+            </li>
+            <li>
+              <a href="#aloqa" className="nav__link subtitle">
+                Aloqa
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="header__text">
+          <h1 className="heading--1 mr-2">Abituriyentlikdan studentlik tomon</h1>
+          <p className="body--2 mr-4">
+            Uyda qoling, online ta’lim oling. At massa at dictumst odio sapien
+            vulputate tortor auctor sollicitudin.
+          </p>
+          <a href="https://t.me/ansortalim" className="btn">
+            Batafsil
+          </a>
+        </div>
       </header>
-    </div>
+    </>
   );
 }
 
